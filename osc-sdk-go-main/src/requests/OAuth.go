6ce1063aaab88f0bbf9f4f933e938bc89a5b5057
Alexandre <alexandrec@alu.ufc.br>
2022-11-19 13:25:41 -0300

    Modificações Features OAuth

--- osc-sdk-go-main/src/requests/OAuth.go
+++ osc-sdk-go-main/src/requests/OAuth.go
@@ -2,98 +2,66 @@ package requests
 
 import (
 	"encoding/base64"
-	"errors"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"modulo/src/domains"
-	jsonUtil "modulo/src/utils/json"
-	"modulo/src/utils/jwt"
 	"net/http"
 	"strings"
 )
+
+var DataBase = domains.Auth{
+	Client_id:     "4dbe3aa7-8ce9-43a4-9298-73b700e712bb",
+	Client_secret: "1b364af124250aa09461f33161c3d96e551d822080fe1bd977aa66d7ec9378c8",
+	Scopes:        []string{"api-external"},
+}
 
 type AuthResponse struct {
 	Access_token string `json:"access_token"`
 	Expire_at    string `json:"expire_at"`
 }
 
-type AuthorizationRequest struct {
-	Client_id string `json:"client_id"`
-	Username  string `json:"username"`
-	Password  string `json:"password"`
+func convertToBase64(auth domains.Auth) string {
+	return base64.StdEncoding.EncodeToString([]byte(auth.Client_id + ":" + auth.Client_secret))
 }
 
-type AuthBodyRequest struct {
-	Scopes []string `json:"scopes"`
-}
+func OAuth() AuthResponse {
+	url := "https://auth.easycredito.com.br/client/auth"
+	method := "POST"
+	payload := strings.NewReader(`{"scopes":["api-external"]}`)
 
-var userDataBase = domains.Auth{
-	Client_id:     "104",
-	Client_secret: "4dbe3aa7-8ce9-43a4-9298-73b700e712bb:1b364af124250aa09461f33161c3d96e551d822080fe1bd977aa66d7ec9378c8",
-	Scopes:        []string{"api-external"},
-}
-
-type ErrorResponse struct {
-	Message string `json:"message"`
-}
-
-func Auth(writer http.ResponseWriter, request *http.Request) {
-
-	writer.Header().Set("Content-Type", "application/json")
-
-	var user, err = getUserByCredentiais(request.Header)
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, payload)
 
 	if err != nil {
-		fmt.Fprintln(writer, jsonUtil.Encode(ErrorResponse{Message: err.Error()}))
-
-		return
+		fmt.Println(err)
+		return AuthResponse{}
 	}
 
-	reqBody, _ := ioutil.ReadAll(request.Body)
-	var post AuthBodyRequest
-	jsonUtil.Decode(reqBody, &post)
+	req.Header.Add("Authorization", "Basic "+convertToBase64(DataBase))
 
-	var token = jwt.GenerateToken(user.Client_id, user.Username, post.Scopes)
+	req.Header.Add("Content-Type", "application/json")
 
-	var authResponse = AuthResponse{
-		Access_token: token.Access_token,
-		Expire_at:    token.Expire_at,
+	res, err = client.Do(req)
+
+	if err != nil {
+		fmt.Println(err)
+		return AuthResponse{}
 	}
 
-	fmt.Fprintln(writer, jsonUtil.Encode(authResponse))
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
 
-}
-
-func getUserByCredentiais(header http.Header) (AuthorizationRequest, error) {
-
-	var response = header.Get("Authorization")
-
-	if response == "" {
-		return AuthorizationRequest{}, errors.New("Internal Server Error")
+	if err != nil {
+		fmt.Println(err)
+		return AuthResponse{}
 	}
+	fmt.Println(string(body))
 
-	sUserB64, _ := base64.StdEncoding.DecodeString(removeBasic(response))
+	var authreponse AuthResponse
 
-	if string(sUserB64) != userDataBase.Client_secret {
+	json.Unmarshal(body, &authreponse)
 
-		return AuthorizationRequest{}, errors.New("User not found")
-	}
+	return authreponse
 
-	return userFromBase64(string(sUserB64)), nil
-
-}
-
-func removeBasic(response string) string {
-	return strings.Replace(response, "Basic ", "", 1)
-}
-
-func userFromBase64(userB64 string) AuthorizationRequest {
-
-	var decodedUser = strings.Split(userB64, ":")
-
-	return AuthorizationRequest{
-		Client_id: userDataBase.Client_id,
-		Username:  decodedUser[0],
-		Password:  decodedUser[1],
-	}
 }
